a656e9b002fe0b478bf3ada632ab821b6a4e79d5
Setembru <weeblyjapa@gmail.com>
2021-03-22 20:02:31 -0300

    Aula 5

---
+++ Aula 5/Bonus 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    char c = 0;
+
+    do {
+        printf("\nInsira a letra s: ");
+        c = getche();
+    } while (c != 's');
+
+    return;
+}
